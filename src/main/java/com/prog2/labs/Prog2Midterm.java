40c990602fd1b7302384cb276ecac54cfb717340
dawodahmadi <ahmadi.dawod@gmail.com>
2023-03-03 12:26:48 -0500

    commit sourced file

--- src/main/java/com/prog2/labs/Prog2Midterm.java
+++ src/main/java/com/prog2/labs/Prog2Midterm.java
@@ -20,7 +20,31 @@ public class Prog2Midterm {
 	 */
 	
     public static ArrayList<ArrayList<Integer>> createPascalTriangle(int numRows) {
-      return new ArrayList<>();
+        ArrayList<ArrayList<Integer>> triangle = new ArrayList<>();
+        
+        if (numRows == 0) {
+            return triangle;
+        }
+        ArrayList<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        triangle.add(firstRow);
+        
+        for (int i = 1; i < numRows; i++) {
+            ArrayList<Integer> row = new ArrayList<>();
+            row.add(1);
+        
+        
+        for (int j =1 ; j < i; j++) {
+            ArrayList<Integer> prevRow = triangle.get(i -1);
+            int num = prevRow.get(j -1) + prevRow.get(j);
+            row.add(num);
+        }
+        
+        row.add(1);
+        triangle.add(row);
+    
+      
     }
-
+    return triangle;
+    }
 }
